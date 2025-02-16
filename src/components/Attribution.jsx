975341eeb73d3029230e8ebb11bce46218c843b9
pkthunder87 <pkthunder87@gmail.com>
2025-02-16 05:17:54 -0800

    Feat; Add Attribution.jsx component

---
+++ src/components/Attribution.jsx
@@ -0,0 +1,23 @@
+function Attribution() {
+  return (
+    <div className="attribution">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.frontendmentor.io/challenges/sunnyside-agency-landing-page-7yVs3B6ef"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/pkthunder87/agency-landing"
+      >
+        PkThunder.
+      </a>
+    </div>
+  );
+}
+
+export default Attribution;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,12 @@
+import Attribution from "./Attribution";
+
 function Footer() {
-  return <footer className="Agency__footer">Footer</footer>;
+  return (
+    <footer className="agency__footer">
+      <div>Footer</div>
+      <Attribution />
+    </footer>
+  );
 }
 
 export default Footer;
